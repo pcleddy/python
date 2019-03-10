6fc8d9fdeed2651f2f0b040e653bef0d5fffeade
Paul Leddy <pcleddy@gmail.com>
2019-03-10 15:35:56 -0700

    Mock ok

--- testing/test003/tests/test_todos.py
+++ testing/test003/tests/test_todos.py
@@ -1,7 +1,10 @@
+from unittest.mock import Mock, patch
 from nose.tools import assert_is_not_none
 from test003.services import get_todos
 
 
-def test_request_response():
+@patch('test003.services.requests.get')
+def test_getting_todos(mock_get):
+    mock_get.return_value.ok = True
     response = get_todos()
     assert_is_not_none(response)
