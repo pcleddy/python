d9a42ac8ea715719ee3481bde6b23eac36c26855
Paul Leddy <pcleddy@gmail.com>
2019-03-10 15:50:06 -0700

    Mock get_todos

--- testing/test003/tests/test_todos.py
+++ testing/test003/tests/test_todos.py
@@ -1,10 +1,25 @@
 from unittest.mock import Mock, patch
-from nose.tools import assert_is_not_none
+from nose.tools import assert_is_none, assert_list_equal
 from test003.services import get_todos
 
 
-def test_getting_todos():
-    with patch('test003.services.requests.get') as mock_get:
-        mock_get.return_value.ok = True
-        response = get_todos()
-    assert_is_not_none(response)
+@patch('test003.services.requests.get')
+def test_getting_todos_when_response_is_ok(mock_get):
+    todos = [{
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }]
+
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = todos
+    response = get_todos()
+    assert_list_equal(response.json(), todos)
+
+
+@patch('test003.services.requests.get')
+def test_getting_todos_when_response_is_not_ok(mock_get):
+    mock_get.return_value.ok = False
+    response = get_todos()
+    assert_is_none(response)
